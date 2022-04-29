51030d3fd9d80d8a7cc1f1d8bb975fa42823558d
logicwang <1758555877@qq.com>
2022-04-29 15:58:27 +0800

    es6学习

---
+++ 变量的解构赋值demo/demo1.js
@@ -0,0 +1,16 @@
+//返回一个数组
+
+function example() {
+    return [1, 2, 3];
+}
+let [a, b, c] = example();
+
+// 返回一个对象
+
+function example() {
+    return {
+        foo: 1,
+        bar: 2
+    };
+}
+let { foo, bar } = example();
