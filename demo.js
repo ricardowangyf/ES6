5ea0d882b74f94771caa31922097122ce09970ab
logicwang <1758555877@qq.com>
2022-04-30 22:45:50 +0800

    提交

---
+++ demo.js
@@ -0,0 +1,18 @@
+var obj = {
+    name: "abc",
+    org: "yellow"
+}
+
+function format(str) {
+    return str
+}
+
+console.log(format(obj))
+    // =>  "'name+org' = 'abc,yellow'"
+    // => ['name', 'org']
+
+// => ['abc', 'yellow']
+
+// => abc+yellow
+
+//key value
--- 变量的解构赋值demo/demo6.js
+++ 变量的解构赋值demo/demo6.js
@@ -1,3 +1,25 @@
+[1, undefined, 3].map((x = 'yes') => x);
+(3)[1, 'yes', 3]
+
+function move({ x, y } = { x: 0, y: 0 }) {
+    return [x, y];
+}
+
+move({ x: 3, y: 8 }); // [3, 8]
+move({ x: 3 }); // [3, undefined]
+move({}); // [undefined, undefined]
+move(); // [0, 0]
+(2)[0, 0]
+
+
+function add([x, y]) {
+    return x + y;
+}
+
+add([1, 2]);
+3
+
+
 function add([x, y]) {
     return x + y;
 }
@@ -12,3 +34,4 @@ add([1, 2]); // 3
     [3, 4]
 ].map(([a, b]) => a + b);
 // 控制台输出结果为 ：[ 3, 7 ]
+//函数参数的解构赋值
