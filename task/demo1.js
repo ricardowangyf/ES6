2230ee5f34bc8da8d952d289459fa151605864ed
logicwang <1758555877@qq.com>
2022-05-02 22:18:26 +0800

    提交

---
+++ task/demo1.js
@@ -0,0 +1,14 @@
+function zhuanhuan9(arr) {
+    console.log('arr', arr)
+    console.log('3333')
+
+    // let bbb = []
+
+    // return 
+    arr.forEach(function(bbb) {
+        console.log(bbb)
+    });
+
+}
+
+console.log(result)
