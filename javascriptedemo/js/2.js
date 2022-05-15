88838f65783ce245101e60b10bef173c61a29f50
logicwang <1758555877@qq.com>
2022-05-15 15:40:51 +0800

    字符串' :'

---
+++ javascriptedemo/js/2.js
@@ -0,0 +1,14 @@
+var carname1 = "Volvo XC60";
+var carname2 = 'Volvo XC60';
+var answer1 = "It's alright";
+var answer2 = "He is called 'Johnny'";
+var answer3 = 'He is called "Johnny"';
+var answer4 = 'It\'s alright';
+var answer5 = "He is called \"Johnny\"";
+console.log(carname1 + "<br>")
+console.log(carname2 + "<br>")
+console.log(answer1 + "<br>")
+console.log(answer2 + "<br>")
+console.log(answer3 + "<br>")
+console.log(answer4 + "<br>")
+console.log(answer5 + "<br>")
