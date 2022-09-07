42f8b57b2092f8a95d9d28e491c6cc049f2ce340
logicwang <1758555877@qq.com>
2022-09-07 21:33:43 +0800

    整理仓库

--- javascriptedemo/js/1.js
+++
@@ -1,7 +0,0 @@
-var pi = 3.14;
-var name = "Bill Gates";
-var answer = 'Yes I am!';
-document.write(pi + "<br>");
-document.write(name + "<br>");
-document.write(answer + "<br>");
-// 向变量分配文本值时，应该用双引号或单引号包围这个值；向变量赋的值是数值时，不要使用引号。如果用引号包围数值，该值会被作为文本来处理。
